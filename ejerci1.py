bdd0a4a656956c90242f732465a4e9854dc4d564
irivillarruel <irivillarruel9@gmail.com>
2023-07-05 09:57:43 -0300

    algo

---
+++ ejerci1.py
@@ -0,0 +1,8 @@
+print ("ejercicio: calcular distancia")
+       
+velocidad = int(input("ingrese velocidad: "))
+tiempo = int(input("ingrese tiempo: "))
+distancia = velocidad*tiempo
+
+                              
+    
